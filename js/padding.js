2b5c4b95e20e89d8202c4ae7998f1b99e5fee1ef
Alexandr Tikhonov <t9101029991@gmail.com>
2016-07-28 12:19:58 +0300

    new gallery

--- js/padding.js
+++ js/padding.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	navHeight = $(".navbar-default").height()+20;
+	navHeight = $(".navbar-default").height()+79;
 	$(".fotorama_wrapper").css("margin-top",navHeight+"px");
 	console.log(navHeight);
 });
